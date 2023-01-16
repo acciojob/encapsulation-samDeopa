34a35c92649da96979e4470ee5a6f973dad70524
samdeopa <you@example.com>
2023-01-16 13:22:35 +0530

    created class for RWOnly and set its name var private

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
-
+//java: name has private access in com.driver.RWOnly
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj  = new RWOnly();
+        obj.setName("Sameer");
+        obj.getName();
+
+    }
 }
